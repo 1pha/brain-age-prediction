890ceb9aa05baa460d32fc9dbf0db75c1e73d657
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-10-13 11:43:28 +0900

    [Create] Levakov model

---
+++ src/model/paper_ieee/levakov.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+
+
+class Levakov(nn.Module):
+
+    def __init__(self, task_type):
+        super(Levakov, self).__init__()
+        self.task_type = task_type
+
+        self.BN = nn.BatchNorm3d(1)
+        self.layer1 = nn.Sequential(
+            nn.Conv3d(1, 8, 3), nn.ReLU(),
+            nn.Conv3d(8, 8, 3), nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2),
+            nn.BatchNorm3d(8)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv3d(8,  16, 3), nn.ReLU(),
+            nn.Conv3d(16, 16, 3), nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2),
+            nn.BatchNorm3d(16),
+            nn.Dropout(.3)
+        )
+
+        self.fc1 = nn.Linear(32, 32)
+        self.fc2 = nn.Linear(32, 1)
+        self.dropout = nn.Dropout(.3)
+
+
+    def forward(self, x):
+        x = self.BN(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+
+        x = x.reshape(x.size(0), -1)
+
+        x = nn.ReLU(self.fc1(x))
+        x = nn.ReLU(self.fc2(x))
+        x = self.dropout(x)
+        if self.task_type == 'binary':
+            x = torch.sigmoid(x)
+
+        return x
