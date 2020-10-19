d520f857f7d3826808fd584fea082143bc2761f4
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-10-19 20:05:42 +0900

    [Run] 1. models with best results / 2. dropouts for fc

--- src/model/paper_ieee/levakov.py
+++ src/model/paper_ieee/levakov.py
@@ -41,7 +41,7 @@ class Levakov(nn.Module):
 
 
     def forward(self, x):
-        
+
         x = self.BN(x)
         x = self.layer1(x)
         x = self.layer2(x)
@@ -50,10 +50,11 @@ class Levakov(nn.Module):
         x = x.reshape(x.size(0), -1)
 
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = torch.sigmoid(self.fc4(x))
-        
         x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc4(x))
 
         return x
