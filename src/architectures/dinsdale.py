f667ddeba9e9e6300724679c4cad4be6a06d42b2
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-12-09 15:12:16 +0900

    [Dinsdale] model added. haven't debugged yet(due to GPU limits)

---
+++ src/architectures/dinsdale.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Dinsdale(nn.Module):
+
+    def __init__(self, in_channels, num_classes):
+
+        super(Dinsdale, self).__init__()
+
+        self.block1 = nn.Sequential(
+            BasicConv3d(in_channels, 32),
+            BasicConv3d(32, 32, max_pool=True)
+        )
+
+        self.block2 = nn.Sequential(
+            BasicConv3d(32, 64),
+            BasicConv3d(64, 64, max_pool=True)
+        )
+
+        self.block3 = nn.Sequential(
+            BasicConv3d(64, 64),
+            BasicConv3d(64, 64, max_pool=True)
+        )
+
+        self.block4 = nn.Sequential(
+            BasicConv3d(64, 64),
+            BasicConv3d(64, 64),
+            BasicConv3d(64, 64, max_pool=True)
+        )
+
+        self.block5 = nn.Sequential(
+            BasicConv3d(64, 96),
+            BasicConv3d(96, 96, ada_pool=True)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(1024, 1024),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+
+        x = torch.flatten(x, 1)
+
+        x = self.fc(x)
+
+        return x
+
+
+class BasicConv3d(nn.Module):
+
+    def __init__(self, in_channels, out_channels, max_pool=False, ada_pool=False):
+
+        super(BasicConv3d, self).__init__()
+
+        self.Conv3d_1  = nn.Conv3d(in_channels,  out_channels, 3, padding=1)
+        # self.Conv3d_2  = nn.Conv3d(out_channels, out_channels, 3, padding=1)
+        self.batchnorm = nn.BatchNorm3d(out_channels)
+
+        self.max_pool = max_pool
+        if self.max_pool:
+            self.maxpool = nn.MaxPool3d(2, 2)
+
+        self.ada_pool = ada_pool
+        if self.ada_pool:
+            self.adapool = nn.AdaptiveAvgPool3d(1024)
+
+    def forward(self, x):
+
+        x = self.Conv3d_1(x)
+        # x = self.Conv3d_2(x)
+        x = self.batchnorm(x)
+
+        if self.max_pool:
+            x = self.maxpool(x)
+
+        if self.ada_pool:
+            x = self.adapool(x)
+
+        return F.relu(x, inplace=True)
