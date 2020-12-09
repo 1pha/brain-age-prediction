4b76e3b7287bc4a7480dfaf571edf99cb3bfa55f
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-12-09 22:06:04 +0900

    [Dinsdale] still working

--- src/architectures/dinsdale.py
+++ src/architectures/dinsdale.py
@@ -4,39 +4,47 @@ import torch.nn.functional as F
 
 class Dinsdale(nn.Module):
 
-    def __init__(self, in_channels, num_classes):
+    def __init__(self, in_channels, num_classes, filters=None):
 
         super(Dinsdale, self).__init__()
 
+        if filters is None:
+            self.filters = [32, 64, 64, 64, 96]
+
+        else:
+            self.filters = filters
+
+        f1, f2, f3, f4, f5 = self.filters
+
         self.block1 = nn.Sequential(
-            BasicConv3d(in_channels, 32),
-            BasicConv3d(32, 32, max_pool=True)
+            BasicConv3d(in_channels, f1),
+            BasicConv3d(f1, f1, max_pool=True)
         )
 
         self.block2 = nn.Sequential(
-            BasicConv3d(32, 64),
-            BasicConv3d(64, 64, max_pool=True)
+            BasicConv3d(f1, f2),
+            BasicConv3d(f2, f2, max_pool=True)
         )
 
         self.block3 = nn.Sequential(
-            BasicConv3d(64, 64),
-            BasicConv3d(64, 64, max_pool=True)
+            BasicConv3d(f2, f3),
+            BasicConv3d(f3, f3, max_pool=True)
         )
 
         self.block4 = nn.Sequential(
-            BasicConv3d(64, 64),
-            BasicConv3d(64, 64),
-            BasicConv3d(64, 64, max_pool=True)
+            BasicConv3d(f3, f4),
+            BasicConv3d(f4, f4),
+            BasicConv3d(f4, f4, max_pool=True)
         )
 
         self.block5 = nn.Sequential(
-            BasicConv3d(64, 96),
-            BasicConv3d(96, 96, ada_pool=True)
+            BasicConv3d(f4, f5),
+            BasicConv3d(f5, f5, ada_pool=True)
         )
 
         self.fc = nn.Sequential(
-            nn.Linear(1024, 1024),
-            nn.Linear(1024, num_classes)
+            nn.Linear(1024, 512),
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, x):
