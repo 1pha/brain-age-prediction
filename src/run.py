8d1e1296b91c31475bbf05fe4f6caa59a14c4417
Daehyun Cho <1phantasmas@korea.ac.kr>
2021-01-06 15:38:34 +0900

    [decorator] remove for now. works fine with streaming offline usage

--- src/run.py
+++ src/run.py
@@ -19,7 +19,7 @@ def logging_time(original_fn):
         start_time = time.time()
         result = original_fn(*args, **kwargs)
         end_time = time.time()
-        print("WorkingTime[{}]: {} sec".format(original_fn.__name__, end_time-start_time))
+        # print("WorkingTime[{}]: {} sec".format(original_fn.__name__, end_time-start_time))
         return result
     return wrapper_fn
 
