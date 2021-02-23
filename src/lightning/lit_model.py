fe95d8e8c6bbf60b3d7faa69e911500dde9cb309
1pha <1phantasmas@korea.ac.kr>
2021-02-23 16:20:32 +0900

    [lightning] errg... smells shit

--- src/lightning/lit_model.py
+++ src/lightning/lit_model.py
@@ -15,14 +15,22 @@ class BaseLitModel(pl.LightningModule):
 
         self.lr = self.cfg.learning_rate
 
+        self.logs = {
+            'train_loss': [],
+            'train_mae': [],
+            'val_loss': [],
+            'val_mae': [],
+        }
+
+        self.resize = tuple(self.cfg['resize'])
         self.train_acc = pl.metrics.regression.MeanAbsoluteError()
         self.val_acc = pl.metrics.regression.MeanAbsoluteError()
         self.test_acc = pl.metrics.regression.MeanAbsoluteError()
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.cfg.learning_rate)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5)
-        return optimizer, scheduler
+        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5)
+        return optimizer#, scheduler
 
     def loss_fn(self, logits, y):
 
@@ -31,7 +39,6 @@ class BaseLitModel(pl.LightningModule):
             l2_reg = torch.tensor(0.).type_as(loss)
             for param in self.parameters():
                 l2_reg += torch.norm(param)
-            
             loss += self.cfg.lamb * l2_reg
         return loss
 
@@ -41,7 +48,8 @@ class BaseLitModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         x, y = batch
         if self.cfg.resize:
-            x = F.interpolate(x, size=(96, 96, 96))
+            # x = F.interpolate(x, size=(96, 96, 96))
+            x = F.interpolate(x, size=self.resize)
         
         logits = self(x).squeeze(1)
         loss = self.loss_fn(logits, y)
@@ -50,10 +58,21 @@ class BaseLitModel(pl.LightningModule):
         self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
         return loss
 
+    def training_epoch_end__(self, outputs):
+        if outputs:
+            print(outputs.keys())
+            trn_acc_mean = torch.stack([x['train_acc'] for x in outputs]).mean()
+            trn_loss_mean = torch.stack([x['train_loss'] for x in outputs]).mean()
+            self.log("train_acc_epoch", trn_acc_mean)
+            self.log("train_loss_epoch", trn_loss_mean)
+        else:
+            pass
+
     def validation_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         x, y = batch
         if self.cfg.resize:
-            x = F.interpolate(x, size=(96, 96, 96))
+            # x = F.interpolate(x, size=(96, 96, 96))
+            x = F.interpolate(x, size=self.resize)
 
         logits = self(x).squeeze(1)
         loss = self.loss_fn(logits, y)
@@ -61,11 +80,24 @@ class BaseLitModel(pl.LightningModule):
         self.val_acc(logits, y)
         self.log("val_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
 
+    def validation_epoch_end(self, outputs):
+        if outputs:
+            val_acc_mean = torch.stack([x['val_acc'] for x in outputs]).mean()
+            val_loss_mean = torch.stack([x['val_loss'] for x in outputs]).mean()
+            self.log("val_acc_epoch", val_acc_mean)
+            self.log("val_loss_epoch", val_loss_mean)
+            return {'val_acc': val_acc_mean}
+
+        else:
+            pass
+
     def test_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         x, y = batch
         if self.cfg.resize:
-            x = F.interpolate(x, size=(96, 96, 96))
+            # x = F.interpolate(x, size=(96, 96, 96))
+            x = F.interpolate(x, size=self.resize)
 
         logits = self(x).squeeze(1)
         self.test_acc(logits, y)
         self.log("test_acc", self.test_acc, on_step=False, on_epoch=True)
+        
