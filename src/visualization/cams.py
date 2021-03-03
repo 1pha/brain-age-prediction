d5c1c427a1a4f1980a861288e6b2a1d69a29ca7c
1pha <1phantasmas@korea.ac.kr>
2021-03-03 14:55:21 +0900

    [cam] perfect workflow. needs revisit in algorithm

--- src/visualization/cams.py
+++ src/visualization/cams.py
@@ -21,24 +21,43 @@ class CAM:
 
         self.hooks = dict()
         if model_name == 'resnet':
-            conv_layers = [
-                    self.model.conv1,
-                    self.model.layer1[0].conv1,
-                    self.model.layer1[0].conv2,
-                    self.model.layer2[0].conv1,
-                    self.model.layer2[0].conv2,
-                    self.model.layer3[0].conv1,
-                    self.model.layer3[0].conv2,
-                    self.model.layer4[0].conv1,
-                    self.model.layer4[0].conv2,
-                    ]
+            self.conv_layers = [
+                self.model.conv1,
+                self.model.layer1[0].conv1,
+                self.model.layer1[0].conv2,
+                self.model.layer2[0].conv1,
+                self.model.layer2[0].conv2,
+                self.model.layer3[0].conv1,
+                self.model.layer3[0].conv2,
+                self.model.layer4[0].conv1,
+                self.model.layer4[0].conv2,
+            ]
+            self.conv_layers = {
+                'conv1': self.model.conv1,
+                'layer1_conv1': self.model.layer1[0].conv1,
+                'layer1_conv2': self.model.layer1[0].conv2,
+                'layer2_conv1': self.model.layer2[0].conv1,
+                'layer2_conv2': self.model.layer2[0].conv2,
+                'layer3_conv1': self.model.layer3[0].conv1,
+                'layer3_conv2': self.model.layer3[0].conv2,
+                'layer4_conv1': self.model.layer4[0].conv1,
+                'layer4_conv2': self.model.layer4[0].conv2,
+            }
 
-            for i, layer in enumerate(conv_layers):
-                self.hooks[i] = layer.register_backward_hook(self.save_gradient)
-        
+
+        elif model_name == 'CNN':
+            self.conv_layers = [
+                self.model.layer[0],
+                self.model.layer[3],
+                self.model.layer[7],
+                self.model.layer[11],
+            ]
+
         else:
             raise NotImplementedError
-            
+
+        for i, layer in enumerate(self.conv_layers.values()):
+            self.hooks[i] = layer.register_backward_hook(self.save_gradient)
         
     def save_gradient(self, *args):
         grad_input = args[1]
@@ -59,24 +78,37 @@ class CAM:
     
     def visualize(self, slc=48):
 
-        fig, ax = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
-
         self.resized_cams = self.resizing() if self.resized_cams is None else self.resized_cams
-        for idx, cam_ in enumerate(self.resized_cams):
+        if self.cfg.model_name == 'resnet':
 
-            row, col = idx // 3, idx % 3
-            ax[row, col].imshow(cam_[:, slc, :], cmap='gray')
+            fig, ax = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
+            for idx, (cam_, layer) in enumerate(zip(self.resized_cams, self.conv_layers.keys())):
+
+                row, col = idx // 3, idx % 3
+                ax[row, col].imshow(cam_[:, slc, :])
+                ax[row, col].set_title(layer)
+
+        elif self.cfg.model_name == 'CNN':
+
+            fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(15, 15))
+            for idx, cam_ in enumerate(self.resized_cams):
+
+                row, col = idx // 2, idx % 2
+                ax[row, col].imshow(cam_)
                        
-        
-    
     def get_cam(self, idx):
         '''
         Get CAM = ReLU(sum(alpha*grad_cam))
         '''
         grad = self.gradient[idx]
-        alpha = torch.sum(grad,  dim=4, keepdim=True)
-        alpha = torch.sum(alpha, dim=3, keepdim=True)
-        alpha = torch.sum(alpha, dim=2, keepdim=True)
+        if self.cfg.model_name == 'resnet':
+            alpha = torch.sum(grad,  dim=4, keepdim=True)
+            alpha = torch.sum(alpha, dim=3, keepdim=True)
+            alpha = torch.sum(alpha, dim=2, keepdim=True)
+        
+        else:
+            alpha = torch.sum(grad,  dim=3, keepdim=True)
+            alpha = torch.sum(alpha, dim=2, keepdim=True)
         
         cam = alpha * grad
         cam = torch.sum(cam, dim=0)
@@ -90,7 +122,6 @@ class CAM:
         return self.cams
 
     def resizing(self):
-
         self.resized_cams = [resize(c.cpu().numpy(), output_shape=self.cfg.preprocess['resize'])
                             for c in self.cams]
         return self.resized_cams
