138c1d6ec0fc68acbb872d70ff10a8295d44957f
1pha <1phantasmas@korea.ac.kr>
2021-05-11 21:19:30 +0900

    [run, losses] change logic

--- src/training/losses.py
+++ src/training/losses.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from sklearn.metrics import r2_score
 
 class RMSELoss(nn.Module):
     def __init__(self):
@@ -9,8 +10,21 @@ class RMSELoss(nn.Module):
     def forward(self, yhat, y):
         return torch.sqrt(self.mse(yhat, y))
 
-fn_lst = {
-    'mse': nn.MSELoss(),
-    'rmse': RMSELoss(),
-    'mae': nn.L1Loss()
-}
+def get_metric(y_pred, y, metric: str):
+
+    metric = metric.lower()
+    if metric == 'r2':
+        return r2_score(y, y_pred)
+    return {
+        'mse': nn.MSELoss(),
+        'rmse': RMSELoss(),
+        'mae': nn.L1Loss()
+    }[metric](y_pred, y).item()
+
+
+if __name__=="__main__":
+
+    y_pred = torch.tensor([1, 2, 3], dtype=torch.float16)
+    y_true = torch.tensor([4, 5, 6], dtype=torch.float16)
+
+    print(get_metric(y_pred, y_true, 'r2'))
--- src/training/run.py
+++ src/training/run.py
@@ -1,25 +1,13 @@
 import time
-from itertools import chain
-from datetime import datetime
-
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix as cf
-from sklearn.metrics import classification_report
 
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from ..data.data_util import * 
-from .losses import RMSELoss, fn_lst
+
+from .losses import get_metric
+from ..data.data_util import get_dataloader 
 from ..models.model_util import load_model, save_checkpoint
-
-import sys
-sys.path.append('../')
-from DeepNotion.build import write_db
-
 
 def logging_time(original_fn):
 
@@ -42,83 +30,57 @@ def make_df(data, label):
         'Label': [label] * len(trues)
     })
 
+class AgeTrainer:
+
+    def __init__(self, cfg):
+
+        pass
+
+
 def run(cfg, fold, db=None, mlflow=None):
 
     model, cfg.device = load_model(cfg.model_name, verbose=False)
+
+    # TODO add optimizer config
     optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate)
 
-    trn_dp, aug_dp, tst_dp = DataPacket(), DataPacket(), DataPacket()
-    aug_dp.delete('corr')
-
-    best_mae = cfg.best_mae
+    best_mae = -1
     fold = None
     for e in range(cfg.epochs):
         
-        start_time = time.time()
-        print(f'Epoch {e+1} / {cfg.epochs}, BEST MAE {best_mae:.3f}')
-        cfg.test = False
-        model, trn_dp, trn_res = train(model, optimizer, fn_lst, trn_dp, cfg, fold=fold)
-        model, aug_dp, aug_res = train(model, optimizer, fn_lst, aug_dp, cfg, fold=fold, augment=True)
-        cfg.test = True
-        model, tst_dp, tst_res = eval(model, fn_lst, tst_dp, cfg, fold=fold)
-        elapsed_time = round(time.time() - start_time, 3)
+        print(f'Epoch {e + 1} / {cfg.epochs}, BEST MAE {best_mae:.3f}')
+        trn_metrics, trn_res = train(model, optimizer, cfg)
+        aug_metrics, aug_res = train(model, optimizer, cfg, augment=True)
+        val_metrics, tst_res = valid(model, cfg, fold=fold)
         
-        if best_mae > tst_dp.mae[-1]:
+        if best_mae < val_metrics['mae']:
             
-            best_mae = tst_dp.mae[-1]
+            best_mae = val_metrics['mae']
             model_name = f'{cfg.model_name}_ep{e}-{cfg.epochs}_sd{cfg.seed}_mae{best_mae:.3f}.pt'
             save_checkpoint(cfg.get_dict(), model_name, is_best=True)
             
         df = pd.concat([make_df(trn_res, 'Train'),
                         make_df(aug_res, 'Aug'),
                         make_df(tst_res, 'Valid')], ignore_index=True)
-        
-        trn_dp.corr.update(df[df['Label'] == 'Train'].corr().Prediction['True'])
-        trn_dp.refresh()
-        tst_dp.corr.update(df[df['Label'] == 'Valid'].corr().Prediction['True'])
-        tst_dp.refresh()
-
-        if e % 1 == 0:
-            trn_dp.info('train')
-            aug_dp.info('augme')
-            tst_dp.info('valid')
-
-        if e % 5 == 0:
-            plt.title(f"L1 Losses among epochs, {e}th")
-            plt.plot(list(trn_dp.loss), label='Train')
-            plt.plot(list(tst_dp.loss), label='Valid')
-            plt.grid(); plt.legend()
-
-            sns.lmplot(data=df, x='True', y='Prediction', hue='Label')
-            plt.grid()
-            plt.show()
-
-            if db is not None:
-                data = gather_data(e=e, time=elapsed_time, cfg=cfg,
-                                    train=trn_dp, valid=tst_dp, aug=aug_dp)
-                write_db(db, data)
-            
-        if mlflow:
-            metrics = mlflow_data(time=elapsed_time, train=trn_dp, valid=tst_dp, aug=aug_dp)
-            mlflow.log_metrics(metrics, e)
 
         torch.cuda.empty_cache()
 
     mlflow.end_run()
-    return model, (trn_dp, aug_dp, tst_dp), (trn_res, tst_res)
+
+    return model, (trn_res, tst_res)
+
 
 @logging_time
-def train(model, optimizer, loss_fns, DP, CFG, fold=None, augment=False):
+def train(model, optimizer, loss_fns, CFG, augment=False):
 
-    # dset = MyDataset(CFG, augment=augment, fold=fold)
-    # dset = DatasetPlus(CFG, augment=augment, test=False)    
-    dset = MNIDataset(CFG, augment=augment, test=False)
-    dataloader = DataLoader(dset, batch_size=CFG.batch_size)
+    dataloader = get_dataloader(augment=augment, test=False)
 
     device = CFG.device
 
     model.train()
     predictions, targets = [], []
+    losses = []
+    _metrics = {metric: [] for metric in CFG.metrics}
     for i, (x, y) in enumerate(dataloader):
 
         try: 
@@ -137,16 +99,10 @@ def train(model, optimizer, loss_fns, DP, CFG, fold=None, augment=False):
 
         loss = loss_fns['mse'](y_pred, y)
 
-        if i % 8 == 0 and CFG.debug:
-            fig, ax = plt.subplots()
-            ax.set_title(f"Augment {i}, MAE {loss_fns['mae'](y_pred, y).item()}")
-            ax.imshow(x.detach().cpu()[0][0][:, 48, :].T, cmap='gray', origin='lower')
-            plt.show()
-
         # Track down results
-        DP.loss.batch_update(loss.item(), 1)
-        DP.mae.batch_update(loss_fns['mae'](y_pred, y).item(), 1)
-        DP.rmse.batch_update(loss_fns['rmse'](y_pred, y).item(), 1)
+        losses.append(loss)
+        for metric in CFG.metrics:
+            _metrics[metric].append(get_metric(y_pred, y, metric))
 
         if CFG.lamb:
             l2_reg = torch.tensor(0.).to(device)
@@ -160,29 +116,25 @@ def train(model, optimizer, loss_fns, DP, CFG, fold=None, augment=False):
         del x, y, y_pred
 
     torch.cuda.empty_cache()
-    DP.loss.clear()
-    DP.mae.clear()
-    DP.rmse.clear()
-    DP.refresh()
 
     predictions = torch.cat(predictions).detach().numpy()
     targets = torch.cat(targets).detach().numpy()
+    _metrics = {metric: sum(_v)/len(_v) for metric, _v in _metrics.items()}
 
-    return model, DP, (predictions, targets)
+    return _metrics, (predictions, targets)
 
 
 @logging_time
-def valid(model, loss_fns, DP, CFG, fold=None):
+def valid(CFG):
 
-    # dset = MyDataset(CFG, augment=False, fold=fold)
-    # dset = DatasetPlus(CFG, augment=False, test=True)
-    dset = MNIDataset(CFG, augment=False, test=True)
-    dataloader = DataLoader(dset, batch_size=CFG.batch_size)
+    dataloader = get_dataloader(augment=False, test=True)
 
     device = CFG.device
     
     model.eval()
     predictions, targets = [], []
+    losses = []
+    _metrics = {metric: [] for metric in CFG.metrics}
     with torch.no_grad(): # to not give loads on GPU... :(
         for i, (x, y) in enumerate(dataloader):
 
@@ -200,16 +152,10 @@ def valid(model, loss_fns, DP, CFG, fold=None):
 
             loss = loss_fns['mse'](y_pred, y)
 
-            if i % 3 == 0 and CFG.debug:
-                fig, ax = plt.subplots()
-                ax.set_title(f"Eval {i}, MAE {loss_fns['mae'](y_pred, y).item()}")
-                ax.imshow(x.detach().cpu()[0][0][:, 48, :].T, cmap='gray', origin='lower')
-                plt.show()
-             
             # Track down results
-            DP.loss.batch_update(loss.item(), 1)
-            DP.mae.batch_update(loss_fns['mae'](y_pred, y).item(), 1)
-            DP.rmse.batch_update(loss_fns['rmse'](y_pred, y).item(), 1)
+            losses.append(loss)
+            for metric in CFG.metrics:
+                _metrics[metric].append(get_metric(y_pred, y, metric))
 
             if CFG.lamb:
                 l2_reg = torch.tensor(0.).to(device)
@@ -220,12 +166,9 @@ def valid(model, loss_fns, DP, CFG, fold=None):
             del x, y, y_pred
 
     torch.cuda.empty_cache()
-    DP.loss.clear()
-    DP.mae.clear()
-    DP.rmse.clear()
-    DP.refresh()
 
     predictions = torch.cat(predictions).detach().numpy()
     targets = torch.cat(targets).detach().numpy()
+    _metrics = {metric: sum(_v)/len(_v) for metric, _v in _metrics.items()}
     
-    return model, DP, (predictions, targets)
+    return _metrics, (predictions, targets)
