ff7d808fc29652c42a701aa85bccc854f56bad2f
1pha <1phantasmas@korea.ac.kr>
2021-05-14 18:20:11 +0900

    [config] to easydict. able to save/load

--- sage/config.py
+++ sage/config.py
@@ -1,44 +1,35 @@
 import yaml
+import easydict
+
 
 CONFIG_FILE_PATH = './config.yml'
 
-def write_config(cfg, changes: dict, path=CONFIG_FILE_PATH):
 
-    for key, item in changes.items():
-        cfg[key] = item
+def edict2dict(edict_obj):
+    dict_obj = {}
 
-    with open(path, 'w+') as yml_config_file:
-        yaml.dump(cfg.get_dict(), yml_config_file, default_flow_style=False)
+    for key, vals in edict_obj.items():
+        if isinstance(vals, easydict.EasyDict):
+            dict_obj[key] = edict2dict(vals)
+        else:
+            dict_obj[key] = vals
+
+    return dict_obj 
+
+
+def save_config(cfg, path=CONFIG_FILE_PATH):
+
+    if isinstance(cfg, easydict.EasyDict):
+        cfg = edict2dict(cfg)
+
+    else:
+        pass
+
+    with open(path, 'w') as y:
+        yaml.dump(cfg, y)
 
 
 def load_config(path=CONFIG_FILE_PATH):
 
-    with open(path, 'r') as yml_config_file:
-        return CFG(yaml.load(yml_config_file))
-
-
-class CFG:
-    def __init__(self, cfg):
-        self.cfg_dict = cfg
-        for c in cfg:
-            setattr(self, c, cfg[c])
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __setitem__(self, key, value):
-        self.cfg_dict[key] = value
-        return setattr(self, key, value)
-
-    def refresh(self):
-        for key in self.cfg_dict:
-            self.cfg_dict[key] = getattr(self, key)
-
-    def get_dict(self):
-        return self.cfg_dict
-
-    def keys(self):
-        return self.cfg_dict.keys()
-
-    def items(self):
-        return self.cfg_dict.items()
+    with open(path, 'r') as y:
+        return easydict.EasyDict(yaml.load(y, Loader=yaml.FullLoader))
