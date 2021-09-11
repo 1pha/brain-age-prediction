97957829c8f40a9cec3634c0d0bb3a14b46d6b11
1pha <1phantasmas@korea.ac.kr>
2021-09-11 15:04:34 +0900

    [train] run with script and args

--- train.py
+++ train.py
@@ -1,42 +1,34 @@
-import os
 import wandb
 
 from sage.config import load_config
 from sage.args import parse_args
-from sage.training import runner, unlearner
-from utils.misc import seed_everything, get_today
+from sage.training.trainer import MRITrainer
+
+NAMEDICT = {"vanillaconv": "VanillaConv", "resnet": "ResNet", "convit": "ConViT"}
 
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
+
     cfg = load_config()
+
+    # TODO Need These to be generalized
+    cfg.phase_config = {"epochs": [100], "update": [["reg"]]}
+    cfg.encoder.name = "resnet"
+    cfg.encoder.config.start_channels = 32  # THIS IS THE PROBLEM !!
+
     args = parse_args()
     cfg.update(args)
-    run_name = cfg.run_name if cfg.get('run_name') else 'DEFAULT NAME'
 
-    cfg.registration = 'mni'
-    cfg.unused_src = []
+    trainer = MRITrainer(cfg)
 
-    cfg.unlearn = True
-    cfg.unlearn_cfg.encoder.name = 'resnet'
-
-    cfg.unlearn_cfg.opt_conf.point = 50
-    cfg.epochs = 200
-    cfg.early_patience = 40
-    # cfg.unlearn_cfg.opt_conf.use = False
-    cfg.unlearn_cfg.domainer.num_dbs = 4 - len(cfg.unused_src)
-
-    name = 'RES(128) 4DB CONF50 '
-    cfg.RESULT_PATH = os.path.join(cfg.RESULT_PATH, name + get_today())
-    seed_everything(cfg.seed)
+    run_name = cfg.run_name if cfg.get("run_name") else "DEFAULT NAME"
 
     wandb.login()
     wandb.init(
-        project='3d_smri',
+        project="3d_smri",
         config=vars(cfg),
-        name=name
+        name=run_name,
+        tags=[NAMEDICT[cfg.encoder.name], "baseline", "save", f"seed {cfg.seed}"],
     )
 
-    # model = runner.run(cfg)
-    unlearner.run(cfg)
-    wandb.finish()
+    trainer.run(cfg)
