9518ae5009ce59cacd6e05b17447cdb3d326a24f
1pha <1phantasmas@korea.ac.kr>
2022-01-19 23:24:10 +0900

    <shell> add seed

--- sweep.sh
+++ sweep.sh
@@ -2,5 +2,5 @@
 
 for seed in {42..91};
     do
-    python train.py --registration=non_registered --run_name="ResNet (Base) Non Registered seed$seed Naive" --checkpoint_period=1 --gpu_num=0
+    python train.py --registration=non_registered --run_name="ResNet (Base) Non Registered seed$seed Naive" --checkpoint_period=1 --gpu_num=0 --seed=$seed
 done
