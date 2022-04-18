f74ead73638e89661716f877f4a0d910b4fede38
1pha <1phantasmas@korea.ac.kr>
2022-04-18 22:47:49 +0900

    <delete> remove logger when making path (not necessary)

--- sage/utils/path_utils.py
+++ sage/utils/path_utils.py
@@ -17,7 +17,8 @@ def _generate_name(model_args, data_args, training_args, misc_args):
     return dir_name
 
 
-def set_path(model_args, data_args, training_args, misc_args, logger):
+def set_path(model_args, data_args, training_args, misc_args):
+
 
     if misc_args.output_dir is None:
         misc_args.output_dir = _generate_name(
@@ -27,19 +28,19 @@ def set_path(model_args, data_args, training_args, misc_args, logger):
     # Check if same name folder exists.
     output_fullpath = os.path.join(misc_args.output_path, misc_args.output_dir)
     if os.path.exists(output_fullpath):
-        logger.warn(
+        print(
             f"You're overwriting on a directory {output_fullpath}. Please check"
         )
         if misc_args.overwrite_output:
-            logger.warn(
+            print(
                 f"Overwriting arguments checked to True. We will overwrite on the folder."
             )
             return output_fullpath
 
         else:
-            logger.info(f"Overwriting arguments checked to False. Stop training.")
-            return False
+            print(f"Overwriting arguments checked to False. Stop training.")
+            raise
     else:
         os.makedirs(output_fullpath, exist_ok=True)
-        logger.info(f"No errors found in setting path.")
+        print(f"No errors found in setting path.")
         return output_fullpath
