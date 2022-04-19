0757470fcf85cf8ba33c8ef5cd857683f9f408f0
1pha <1phantasmas@korea.ac.kr>
2022-04-19 13:25:13 +0900

    <fix> scheduler import error fixed

--- sage/training/scheduler.py
+++ sage/training/scheduler.py
@@ -16,9 +16,9 @@ def construct_scheduler(optimizer, training_args, logger=None):
         return scheduler
 
     elif name == "cosine_linear_warmup":
-        from transformers import get_cosine_schedule_warmup
+        from transformers import get_cosine_schedule_with_warmup
 
-        scheduler = get_cosine_schedule_warmup(
+        scheduler = get_cosine_schedule_with_warmup(
             optimizer,
             num_warmup_steps=training_args.warmup_steps,
             num_training_steps=training_args.total_steps,
