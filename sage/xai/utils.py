26c30e28a649d54849a9ed331ceeb3f0e577cda1
1pha <1phantasmas@korea.ac.kr>
2022-06-17 20:51:21 +0900

    U: update utils function for xais

--- sage/xai/utils.py
+++ sage/xai/utils.py
@@ -1,19 +1,29 @@
 import torch
 import nibabel
 import numpy as np
+from nilearn.datasets import load_mni152_template
+
+MNI_AFFINE = load_mni152_template().affine
 
 def z_norm(tensor):
 
-    if tensor.ndim == 5:
+    if tensor.ndim == 5: # (N, 1, 96, 96, 96)
         z_normed = torch.cat([_z_norm(t[0])[None, None, ...] for t in tensor], dim=0)
-        return z_normed
+
+    elif tensor.ndim == 4: # (1, 96, 96, 96)
+        z_normed = _z_norm(tensor[0])
+
+    elif tensor.ndim == 3:
+        z_normed = _z_norm(tensor)
+
+    return z_normed
 
 def _z_norm(tensor):
 
     assert tensor.ndim == 3, f"Give 3-dimensional tensor. Given {tensor.shape}."
 
-    mu = tensor.mean()
-    sigma = tensor.var().sqrt()
+    mu = np.nanmean(tensor)
+    sigma = np.nanstd(tensor)
 
     return (tensor - mu) / sigma
 
@@ -23,3 +33,4 @@ def average(tensor, dim=0):
     return tensor.sum(dim) / N
 
 _nifti = lambda arr: nibabel.nifti1.Nifti1Image(arr, np.eye(4))
+_mni = lambda arr: nibabel.nifti1.Nifti1Image(arr, MNI_AFFINE)
