ef947c6e07ea5f122bc2487367e6393814f56cfc
1pha <1phantasmas@korea.ac.kr>
2023-05-26 08:00:18 +0000

    Add probabilistic mask

--- sage/data/augmentation.py
+++ sage/data/augmentation.py
@@ -3,15 +3,16 @@ import monai.transforms as mt
 
 
 def mask_transform(spatial_size: tuple = (96, 96, 96),
-                   mask: torch.Tensor = None) -> mt.transform:
+                   mask: torch.Tensor = None,
+                   prob: float = 1.0) -> mt.transform:
     """ Checks if mask is valid when given,
     and return monai.transforms masking out masks
     Otherwise, identity transform will be returned. """
     if mask is not None:
         assert mask.squeeze().shape == spatial_size
-        return mt.Lambda(lambda t: t * mask.to(t.device))
+        return mt.RandLambda(lambda t: t * mask.to(t.device), prob=prob)
     else:
-        return mt.Lambda(lambda t: t)
+        return mt.RandLambda(lambda t: t, prob=prob)
 
 
 def no_augment(spatial_size: tuple = (96, 96, 96),
@@ -28,6 +29,22 @@ def no_augment(spatial_size: tuple = (96, 96, 96),
 def augment(spatial_size: tuple = (96, 96, 96),
             mask: torch.Tensor = None):
     apply_mask: mt.transform = mask_transform(spatial_size=spatial_size, mask=mask)
+    return mt.Compose([
+        mt.Resize(spatial_size=spatial_size),
+        mt.ScaleIntensity(),
+        apply_mask,
+        mt.RandAdjustContrast(prob=0.1, gamma=(0.5, 3.5)),
+        mt.RandCoarseDropout(holes=20, spatial_size=8, prob=0.4, fill_value=0.),
+        mt.RandAxisFlip(prob=0.5),
+        mt.RandZoom(prob=0.5, min_zoom=0.9, max_zoom=1.3, mode="trilinear"),
+        mt.Lambda(lambda t: t.unsqueeze(dim=1)),
+    ])
+    
+    
+def augment_mask(spatial_size: tuple = (96, 96, 96),
+                 mask: torch.Tensor = None):
+    """ Using masking as augmentation probabilistically """
+    apply_mask: mt.transform = mask_transform(spatial_size=spatial_size, mask=mask, prob=0.7)
     return mt.Compose([
         mt.Resize(spatial_size=spatial_size),
         mt.ScaleIntensity(),
