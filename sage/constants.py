f5834916ab43533baf3c49e822fdfbb528b41286
1pha <1phantasmas@korea.ac.kr>
2023-06-05 16:47:05 +0900

    Add biobank affine as constants

--- sage/constants.py
+++ sage/constants.py
@@ -1,4 +1,10 @@
 from nilearn.datasets import load_mni152_brain_mask
+import numpy as np
 
 
 MNI_SHAPE = load_mni152_brain_mask().get_fdata().shape
+
+BIOBANK_AFFINE = np.array([[ -1.,  0.,  0.,   90.],
+                           [  0.,  1.,  0., -126.],
+                           [  0.,  0.,  1.,  -72.],
+                           [  0.,  0.,  0.,    1.]], dtype=np.float32)
