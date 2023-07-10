6cc63390bac2315cce640fc88bf72b5dfb752057
1pha <1phantasmas@korea.ac.kr>
2023-07-10 16:32:34 +0200

    Fix with subprocess

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ ENV = os.environ.copy()
 ENV["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 BASE_DIR = Path("/Users/daehyuncho/codespace/brain-age-prediction/fastsurfer")
+FS_DIR = Path("/Users/daehyuncho/FastSurfer")
 
 
 def run_fs(idx: int, ds: Dataset):
@@ -28,19 +29,20 @@ def run_fs(idx: int, ds: Dataset):
     # Run Fastsurfer
     try:
         subprocess.call(["./run_fastsurfer.sh",
-                        "--t1", BASE_DIR / f"tmp_nii/{stem}.nii",
-                        "--sd", BASE_DIR / "seg",
-                        "--sid", stem,
-                        "--seg_only", "--allow_root", "--no_cereb",
-                        "--threads", "1", "--device", "mps"], env=ENV)
+                         "--t1", BASE_DIR / f"tmp_nii/{stem}.nii",
+                         "--sd", BASE_DIR / "seg",
+                         "--sid", stem,
+                         "--seg_only", "--allow_root", "--no_cereb",
+                         "--threads", "1", "--device", "mps"],
+                         env=ENV, cwd=FS_DIR)
         subprocess.call(["rm", BASE_DIR / f"tmp_nii/{stem}.nii"])
         return True
     except:
-        return stem
+        raise
 
 
 def main():
-    subprocess.call(["cd", "/Users/daehyuncho/FastSurfer"])
+    # subprocess.call(["cd", "/Users/daehyuncho/FastSurfer"], shell=True)
     ds = UKBDataset(mode="test")
     with mp.Pool(processes=4) as p:
         p.starmap(run_fs, ((idx, ds) for idx in range(len(ds))))
