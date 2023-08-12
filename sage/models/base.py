7815e7b6e4d25d558d64f1c1b6cf9a7bd5f50a95
1pha <1phantasmas@korea.ac.kr>
2023-08-12 07:11:01 +0000

    Add convnext to repository

--- sage/models/base.py
+++ sage/models/base.py
@@ -1,21 +1,28 @@
 import torch
 from torch import nn
 
+from sage.utils import get_logger
+
+
+logger = get_logger(name=__file__)
+
 
 class ModelBase(nn.Module):
     def __init__(self,
                  backbone: nn.Module,
                  criterion: nn.Module,
-                 name: str):
+                 name: str,
+                 task: str = "reg"):
         super().__init__()
+        logger.info("Start Initiating model %s", name.upper())
         self.backbone = backbone
         self.criterion = criterion
         self.NAME = name
-    
+        self.TASK = task
+
     def forward(self, brain: torch.Tensor, age: torch.Tensor):
         pred = self.backbone(brain).squeeze()
         loss = self.criterion(pred, age)
-        
         return dict(loss=loss,
                     reg_pred=pred.detach().cpu(),
                     reg_target=age.detach().cpu())
@@ -31,3 +38,11 @@ class ModelBase(nn.Module):
             return s
         ckpt = {parse_ckpt(k): v for k, v in ckpt.items()}
         self.load_state_dict(ckpt)
+
+
+class ConvNext(ModelBase):
+    def __init__(self,
+                 backbone: nn.Module,
+                 criterion: nn.Module,
+                 name: str):
+        super().__init__(backbone=backbone, criterion=criterion, name=name)
