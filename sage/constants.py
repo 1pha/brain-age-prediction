87d2e27d566bac17587f12db988d5400c11903c6
1pha <1phantasmas@korea.ac.kr>
2024-03-04 07:09:09 +0000

    Add PPMI dataset configuration and modify dataset base dir to referring dotenv file

--- sage/constants.py
+++ sage/constants.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 from nilearn.datasets import load_mni152_brain_mask
 import numpy as np
+from dotenv import dotenv_values
 
+config = dotenv_values(dotenv_path=".env")
 
 mni_template = load_mni152_brain_mask()
 MNI_SHAPE = mni_template.get_fdata().shape
@@ -17,7 +19,7 @@ BIOBANK_AFFINE = np.array([[ -1.,  0.,  0.,   90.],
 # Reshape target size
 SPATIAL_SIZE = (160, 192, 160)
 
-DATA_BASE = Path.home() / "data" / "hdd01" / "1pha"
+DATA_BASE = Path(config.get("DATA_BASE", Path.home() / "data" / "hdd01" / "1pha"))
 BIOBANK_PATH = DATA_BASE / "h5"
 
 EXT_BASE = DATA_BASE / "brain"
